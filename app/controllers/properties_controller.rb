ea07226055c52a2979476ab6bff2682b8d536622
Ryo_Odo <marukae@gmail.com>
2021-11-22 07:47:46 +0000

    編集、更新未完成

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -6,10 +6,12 @@ class PropertiesController < ApplicationController
 
   def new
     @property = Property.new
+    @property.stations.build
   end
 
   def create
-    if Property.create(property_params)
+    @property = Property.new(property_params)
+    if @property.save
       redirect_to properties_path, notice: "登録しました！"
     else
       render :new
@@ -17,20 +19,33 @@ class PropertiesController < ApplicationController
   end
 
   def show
+    @stations = @property.stations
   end
 
   def edit
+    @count = @property.stations.count
+    @count += 1
+    @property.stations.build
   end
 
   def update
-    if @property.update(property_params)
-      redirect_to properties_path, notice: "物件をを編集しました！"
+    binding.irb
+    if params[:property][:station][:station].blank?
+      if @property.update(property_params)
+        redirect_to properties_path, notice: "物件をを編集しました！"
+      else
+        render :edit
+      end
     else
-      render :edit
+      if @property.save
+        redirect_to properties_path, notice: "登録しました！"
+      end
     end
   end
 
   def destroy
+    @i = Station.where(property_id: @property.id)
+    @i.destroy_all
     @property.destroy
     redirect_to properties_path, notice:"物件を削除しました"
   end
@@ -41,6 +56,6 @@ class PropertiesController < ApplicationController
   end
 
   def property_params
-    params.require(:property).permit(:name, :rent, :address, :age, :note)
+    params.require(:property).permit(:name, :rent, :address, :age, :note, stations_attributes: {})
   end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -3,4 +3,6 @@ class Property < ApplicationRecord
   validates :rent, presence: true
   validates :address, presence: true
   validates :age, presence: true
+  has_many :stations
+  accepts_nested_attributes_for :stations, allow_destroy: true, reject_if: :all_blank, update_only: true
 end
